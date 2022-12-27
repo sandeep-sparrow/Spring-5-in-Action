536936e8a6b5f2763de1994c407869dee72783ae
SANDEEP PRAJAPATI <sandeep.prajapati@dxc.com>
2022-12-28 01:53:09 +0530

    test fixes

--- chp2/taco-cloud/src/test/java/tacos/HomeControllerTest.java
+++ chp2/taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -9,11 +9,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
-@RunWith(SpringRunner.class)
 @RunWith(SpringRunner.class)
 @WebMvcTest()
 
